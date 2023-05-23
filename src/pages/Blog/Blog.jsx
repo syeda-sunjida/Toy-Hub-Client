8e2ff05d875b062da4b390efd835ce02ff28acfe
syeda-sunjida <nikitaferdous1999@gmail.com>
2023-05-23 21:28:04 +0600

    blogs added

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -24,17 +24,20 @@ const Blog = () => {
 
         Session Storage: Similar to local storage, session storage can be used to store tokens. However, the tokens are only available for the duration of the user's browsing session and are cleared when the session ends.
 <br />
+</p>
 
-        Comparing SQL and NoSQL databases:
+        2.ans: <br /> Comparing SQL and NoSQL databases:
 
         SQL (Structured Query Language) databases are based on a relational data model and use SQL for data definition, manipulation, and retrieval. They provide a predefined schema, support for transactions, and enforce ACID (Atomicity, Consistency, Isolation, Durability) properties. SQL databases are ideal for applications with complex relationships and structured data, such as financial systems or inventory management.
 
         NoSQL (Not Only SQL) databases, on the other hand, are designed to handle unstructured, semi-structured, and rapidly changing data. They offer flexible schemas and are horizontally scalable, making them suitable for handling large volumes of data, high traffic, and distributed environments. NoSQL databases are often used in modern web applications, real-time analytics, and scenarios where the schema may evolve over time.
 
-        Express.js is a popular web application framework for Node.js. It provides a set of robust features and middleware for building web applications and APIs. Express.js simplifies the process of handling HTTP requests, routing, and integrating with various middleware and databases.
-
+        <p>
+        3.ans: <br /> Express.js is a popular web application framework for Node.js. It provides a set of robust features and middleware for building web applications and APIs. Express.js simplifies the process of handling HTTP requests, routing, and integrating with various middleware and databases.
+<br /> </p>
+<p> 4. ans: <br />
         NestJS is a progressive, extensible framework for building server-side applications with Node.js. It is built on top of Express.js and offers an opinionated structure and a powerful dependency injection system. NestJS promotes the use of TypeScript and follows the principles of modular architecture, making it suitable for building scalable and maintainable applications.
-
+ 
         MongoDB's aggregate function is a powerful tool for data aggregation and processing in MongoDB, a popular NoSQL database. It allows you to perform complex operations on collections of documents, including grouping, filtering, sorting, joining, and performing calculations. The aggregate function uses a pipeline approach, where multiple stages are chained together to process the data. Each stage in the pipeline performs a specific operation on the data, transforming and reshaping it according to your requirements. The pipeline stages can include operations like $match, $group, $sort, $project, and many others, allowing you to perform sophisticated data manipulations and analysis in MongoDB.
       </p>
     </div>
